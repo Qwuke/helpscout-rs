de72b63772263b35482d63bba0b9e36ce0e4f1c8
Qwuke <tristan.e.mortimer@gmail.com>
2018-01-23 09:54:23 -0500

    Added customers end point

--- src/api/customers.rs
+++ src/api/customers.rs
@@ -1,41 +1,61 @@
 use serde_json;
+use chrono::{DateTime, Utc};
 
 use error::HelpScoutError;
-use client::{Client, Status};
-
-const PREFIX: &'static str = "mailboxes";
+use client::Client;
+use envelope::Collection;
+//use api::person::Person;
+//use api::mailboxes::MailboxRef;
 
 #[derive(Debug, Deserialize)]
-pub struct ItemList<T> {
-    page: i32,
-    pages: i32,
-    count: i32,
-    items: Vec<T>,
+#[serde(rename_all = "lowercase")]
+pub enum CustomerPhotoType {
+    Unknown,
+    Gravatar,
+    Twitter,
+    Facebook,
+    Googleprofile,
+    Googleplus,
+    Linkedin,
 }
 
-//Class representing possible values returned for the customers endpoint for the customer object
 #[derive(Debug, Deserialize)]
+#[serde(rename_all = "lowercase")]
+pub enum CustomerGender {
+    Male,
+    Female,
+    Unknown,
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "camelCase")]
 pub struct Customer {
-    id: String,
-    firstName: String,
-    lastName: String,
-    photoUrl: String,
-    photoType: String,
-    gender: String,
-    age: String,
-    organization:String,
-    jobTitle: String,
-    location: String,
-    createdAt: String,
-    modifiedAt: String,
+    pub id: i32,
+    #[serde(rename = "type")]
+    pub first_name: String,
+    pub last_name: String,
+    pub photo_url: String,
+    pub gender: CustomerGender,
+    pub age: String,
+    pub organization: String,
+    pub job_title: String,
+    pub location: String,
+    pub created_at: DateTime<Utc>,
+    pub modified_at: DateTime<Utc>,
+
+    // Additional fields that appear only when retrieving a single customer
+    // TODO: complete
+    //pub background: String,
+    //pub address: CustomerAddress,
+    //pub social_profiles: Vec<CustomerSocialProfiles>
+    //pub emails: Vec<CustomerEmail>,
+    //pub phones: Vec<CustomerPhone>,
+    //pub chats: Vec<CustomerChat>,
+    //pub websites: Vec<CustomerWebsite>,
 }
 
-pub fn list(client: &Client, mailbox_id: &str) -> Result<(Status, ItemList<Customer>), HelpScoutError> {
-    let (status, res) = client.get(PREFIX, &format!("{}/customers.json", mailbox_id), None)?;
-
-    println!("status {:?}", status);
-    println!("result - {:?}", res);
+pub fn list(client: &Client, mailbox_id: i32) -> Result<Collection<Customer>, HelpScoutError> {
+    let res = client.get(&format!("mailboxes/{}/customers.json", mailbox_id), None)?;
     let customers = serde_json::from_value(res.clone())?;
-
-    Ok((status, customers))
+    Ok(customers)
 }
